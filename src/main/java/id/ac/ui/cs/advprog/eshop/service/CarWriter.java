2f6999bc5d8648f9a24d59905c6d7f48381864bf
satria <satria.aleka@gmail.com>
2026-03-01 15:35:22 +0700

    implement ISP

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/CarWriter.java
@@ -0,0 +1,10 @@
+package id.ac.ui.cs.advprog.eshop.service;
+
+import id.ac.ui.cs.advprog.eshop.model.Car;
+
+
+public interface CarWriter {
+    Car create(Car car);
+    void update(String carId, Car car);
+    void deleteCarById(String carId);
+}
